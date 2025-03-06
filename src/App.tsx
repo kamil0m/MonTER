081f7b1577027cafc1140bead9967364d6f52eba
Kamil Markowski <kamil0markowski@gmail.com>
2025-03-06 18:03:49 +0100

    html basic structure added

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>MyTER</h1>
+      <div>Next trains to Lille</div>
+      <div>12:00</div>
+      <div>13:00</div>
+      <div>14:00</div>
+      <div>Next trains to Ascq</div>
+      <div>12:00</div>
+      <div>13:00</div>
+      <div>14:00</div>
+    </>
+  )
+}
+
+export default App
