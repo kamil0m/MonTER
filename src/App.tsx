4e13dd309bf00f494b5b58b32bb9db71e801315d
Kamil Markowski <kamil0markowski@gmail.com>
2025-03-07 13:04:53 +0100

    an exemple api request added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,19 @@ import './App.css'
 
 function App() {
 
+  // const myPromise = new Promise((resolve, reject) => {})
+
+  // const apiReturn = fetch('https://c18b1a6c-1e4e-46d0-850c-3e442e26cb84@api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:SNCF:87581009/departures')
+fetch('https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:SNCF:87286005/departures', 
+  {
+    headers: {
+      Authorization: 'c18b1a6c-1e4e-46d0-850c-3e442e26cb84'
+    }
+  }).then(resp => console.log(resp)).catch(err => console.log(err))
+
+  // console.log(`answer is : ${apiReturn}`);
+  
+
   return (
     <>
       <h1>MyTER</h1>
---
+++ src/components/Departures.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Departures = () => {
+     const codeStation:number = "87581009"; // Bordaux station code
+}
+
+export default Departures
